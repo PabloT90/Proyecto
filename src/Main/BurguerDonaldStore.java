8e8d61e658d0990f128951cf0c454c1dbb2ae6c6
Pablo <pabloprats18@gmail.com>
2019-04-02 14:25:43 +0200

    Update BurguerDonaldStore.java

--- src/Main/BurguerDonaldStore.java
+++ src/Main/BurguerDonaldStore.java
@@ -18,17 +18,17 @@
 * 4 --> Insertar producto con stock.
 * 5 --> Eliminar producto.
 *
-*   Si se elige la opción de "Consultas", se muestra el siguiente submenú:
+*       Si se elige la opción de "Consultas", se muestra el siguiente submenú:
 *
-*   0 --> Volver al menú anterior.
-*   1 --> Mostrar todos los productos del almacen.
-*   2 --> Mostrar un tipo de producto.
-*   3 --> Mostrar productos veganos.
+*       0 --> Volver al menú anterior.
+*       1 --> Mostrar todos los productos del almacen.
+*       2 --> Mostrar un tipo de producto.
+*       3 --> Mostrar productos veganos.
 *
-*   Si se elige la opción "Modificar stock", se muestra el siguiente submenú:
-*   0 --> Volver al menú anterior.
-*   1 --> Incrementar stock.
-*   2 --> decrementar stock.
+*       Si se elige la opción "Modificar stock", se muestra el siguiente submenú:
+*       0 --> Volver al menú anterior.
+*       1 --> Incrementar stock.
+*       2 --> decrementar stock.
 *
 * Si se elige la opción 2, se muestra el siguiente menú:
 *
@@ -66,40 +66,26 @@
 *
 * PG Nivel: 0
 * Inicio
-*
 *   Repetir
-*
 *       leerYValidarOpcionMenu*
-*
 *       Si opcionMenu != 0
-*
 *           Segun (opcionMenu)
-*
 *               para opcionMenu == 1
 *                   AccionesMenu
 *               para opcionMenu == 2
 *                   AccionesProductos
-*
 *           Fin_segun
-*
 *       Fin_si
-*
 *   Mientras opcionMenu != 0
-*
 * Fin
-*
+
 * PG Nivel: 1
 * AccionesMenu
 * Inicio
-*
 *   Repetir
-*
 *       leerYValidarOpcionSubMenu1*
-*
 *       Si opcionSubMenu1 != 0
-*
 *           Segun (opcionSubMenu1)
-*
 *               para opcionSubMenu1 == 1
 *                   Consultas
 *               para opcionSubMenu1 == 2
@@ -111,24 +97,16 @@
 *               para opcionSubMenu1 == 5
 *                   EliminacionProducto
 *           Fin_si
-*
 *       Fin_si
-*
 *   Mientras opcionSubMenu1 != 0
-*
 * Fin
 *
 * AccionesProductos
 * Inicio
-*
 *   Repetir
-*
 *       leerYValidarOpcionSubMenu2*
-*
 *       Si opcionSubMenu1 != 0
-*
 *           Segun (opcionSubMenu1)
-*
 *               para opcionSubMenu1 == 1
 *                   mostrarMenus*
 *               para opcionSubMenu1 == 2
@@ -140,25 +118,17 @@
 *               para opcionSubMenu1 == 5
 *                   EliminacionMenu
 *           Fin_segun
-*
 *       Fin_si
-*
 *   Mientras opcionSubMenu1 != 0
-*
 * Fin
 *
 * PG Nivel: 2
 * Consultas
 * Inicio
-*
 *   Repetir
-*
 *       leerYValidarSubMenu3*
-*
 *       Si opcionSubMenu2 != 0
-*
 *           Segun (opcionSubMenu2)
-*
 *               para opcionSubMenu2 == 1
 *                   mostrarProductosAlmacen*
 *               para opcionSubMenu2 == 2
@@ -166,138 +136,101 @@
 *               para opcionSubMenu2 == 3
 *                   mostrarProductosVeganos*
 *           Fin_segun
-*
 *       Fin_si
-*
 *   Mientras opcionSubMenu2 != 0
-*
 * Fin
 *
 * ModificacionProducto
 * Inicio
-*
 *   leerYValidarId*
-*
 *   Si el producto existe
-*       modificarMenu*
+*       modificarProducto*
 *   Sino
 *       MensajeExplicatorio1
 *   Fin_si
-*
 * Fin
 *
 * ModificacionStock
 * Inicio
-*
 *   Repetir
-*
 *       leerYValidarOpcionSubMenu4*
-*
 *       Si opcionMenu != 0
-*
 *           Segun (opcionMenu)
-*
 *               para opcionMenu == 1
 *                   IncrementoStock
 *               para opcionMenu == 2
 *                   DecrementoStock
-*
 *           Fin_segun
-*
 *       Fin_si
-*
 *   Mientras opcionMenu != 0
-*
 * Fin
 *
 * InsercionProducto
 * Inicio
-*
 *   leerYValidarProducto*
-*
 *   insertarProducto*
-*
 * Fin
 *
 * EliminacionProducto
 * Inicio
-*
 *   leerYValdarId*
-*
 *   Si existe el producto
 *       eliminarProducto*
 *   Sino
 *       MensajeExplicatorio
 *   Fin_si
-*
 * Fin
 *
 * ModificacionMenu
 * Inicio
-*
 *   leerYValidarId*
-*
 *   Si el menú existe
 *       modificarMenu*
 *   Sino
 *       MensajeExplicatorio3
 *   Fin_si
-*
 * Fin
 *
 * CanjeoMenu
 * Inicio
-*
 *   leerYValidarId*
-*
 *   Si el menú existe
-*
 *       Si el menú contiene algún producto sin stock
 *           MensajeExplicatorio4
 *       Sino
 *           canjearMenu*
 *       Fin_si
-*
 *   Sino
 *       MensajeExplicatorio3
 *   Fin_si
-*
 * Fin
 *
 * InsercionMenu
 * Inicio
-*
 *   leerYValidarMenu*
-*
 *   insertarMenu*
-*
 * Fin
 *
 * EliminacionMenu
 * Inicio
-*
 *   leerYValidarId*
-*
 *   Si el menú existe
 *       eliminarMenu*
 *   Sino
 *       MensajeExplicatorio3
 *   Fin_si
-*
 * Fin
 *
 * PG Nivel: 3
 * MostrarProductosPorTipo
 * Inicio
-*
 *   leerYValidarTipoProducto*
-*
 *   mostrarProductosTipo*
-*
 * Fin
 * */
 
 package Main;
 
 public class BurguerDonaldStore {
+
 }
