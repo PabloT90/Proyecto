a8f7faf87b7cc9bb0fd7d3564e87894814accf8c
Yeray Campanario <thewalkerofdreams@gmail.com>
2019-04-25 13:30:03 +0200

    listo

--- src/Conductores/ConductorValidacionesBurguerDonald.java
+++ src/Conductores/ConductorValidacionesBurguerDonald.java
@@ -10,77 +10,77 @@ public class ConductorValidacionesBurguerDonald {
         ImplStockProducto producto1 = new ImplStockProducto(300, EnumTipo.COMPLEMENTO, 1.0, "Patatas fritas", "A gajos", true, 34);
 
         //leerYValidarOpcionMenu
-        //System.out.println(funcion.leerYValidarOpcionMenu());
+        System.out.println(funcion.leerYValidarOpcionMenu());
 
         //leerYValidarOpcionSubMenu1
-        //System.out.println(funcion.leerYValidarOpcionSubMenu1());
+        System.out.println(funcion.leerYValidarOpcionSubMenu1());
 
         //leerYValidarOpcionSubMenu2
-        //System.out.println(funcion.leerYValidarOpcionSubMenu2());
+        System.out.println(funcion.leerYValidarOpcionSubMenu2());
 
         //leerYValidarOpcionSubMenu3
-        //System.out.println(funcion.leerYValidarOpcionSubMenu3());
+        System.out.println(funcion.leerYValidarOpcionSubMenu3());
 
         //leerYValidarOpcionSubMenu4
-        //System.out.println(funcion.leerYValidarOpcionSubMenu4());
+        System.out.println(funcion.leerYValidarOpcionSubMenu4());
 
         //leerYValidarId
-        //System.out.println(funcion.leerYValidarId());
+        System.out.println(funcion.leerYValidarId());
 
         //leerYValidarTipoProducto
-        //System.out.println(funcion.leerYValidarTipoProducto());
+        System.out.println(funcion.leerYValidarTipoProducto());
 
         //leerYValidarPrecio
-        //System.out.println(funcion.leerYValidarPrecio());
+        System.out.println(funcion.leerYValidarPrecio());
 
         //leerYValidarStock
-        //System.out.println(funcion.leerYValidarStock());
+        System.out.println(funcion.leerYValidarStock());
 
         //leerYValidarNuevoProducto
-        //System.out.println(funcion.leerYValidarNuevoProducto(345));
+        System.out.println(funcion.leerYValidarNuevoProducto(345));
 
         //leerYValidarRespuestaTipo
-        //System.out.println(funcion.leerYValidarRespuestaTipo(producto1));
+        System.out.println(funcion.leerYValidarRespuestaTipo(producto1));
 
         //leerYValidarRespuestaPrecio
-        //System.out.println(funcion.leerYValidarRespuestaPrecio(producto1));
+        System.out.println(funcion.leerYValidarRespuestaPrecio(producto1));
 
         //leerYValidarRespuestaNombre
-        //System.out.println(funcion.leerYValidarRespuestaNombre(producto1));
+        System.out.println(funcion.leerYValidarRespuestaNombre(producto1));
 
         //leerYValidarRespuestaDescripcion
-        //System.out.println(funcion.leerYValidarRespuestaDescripcion(producto1));
+        System.out.println(funcion.leerYValidarRespuestaDescripcion(producto1));
 
         //leerYValidarRespuestaVegano
-        //System.out.println(funcion.leerYValidarRespuestaVegano(producto1));
+        System.out.println(funcion.leerYValidarRespuestaVegano(producto1));
 
         //leerYValidarRespuestaStock
-        //System.out.println(funcion.leerYValidarRespuestaStock(producto1));
+        System.out.println(funcion.leerYValidarRespuestaStock(producto1));
 
         //leerYValidarProductoModificado
-        //System.out.println(funcion.leerYValidarProductoModificado(54));
-        //System.out.println(funcion.leerYValidarProductoModificado(55));
+        System.out.println(funcion.leerYValidarProductoModificado(54));
+        System.out.println(funcion.leerYValidarProductoModificado(55));
 
         //leerYValidarProductoDelAlmacen
-        //System.out.println(funcion.leerYValidarProductoDelAlmacen());
+        System.out.println(funcion.leerYValidarProductoDelAlmacen());
 
         //leerYValidarRespuestaInsercion
-        //System.out.println(funcion.leerYValidarRespuestaInsercion());
+        System.out.println(funcion.leerYValidarRespuestaInsercion());
 
         //LeerYValidarNuevoMenu
-        //System.out.println(funcion.leerYValidarNuevoMenu(1));
+        System.out.println(funcion.leerYValidarNuevoMenu(1));
 
         //leerYValidarNuevoMenu
-        //System.out.println(funcion.leerYValidarNuevoMenu(2));
+        System.out.println(funcion.leerYValidarNuevoMenu(2));
 
         //leerYValidarListaProductos
-        //System.out.println(funcion.leerYValidarListaProductos());
+        System.out.println(funcion.leerYValidarListaProductos());
 
         //leerYValidarRespuestaInsercion
-        //System.out.println(funcion.leerYValidarRespuestaInsercion());
+        System.out.println(funcion.leerYValidarRespuestaInsercion());
 
         //leerYValidarProductoDelAlmacen
-        //System.out.println(funcion.leerYValidarProductoDelAlmacen());
+        System.out.println(funcion.leerYValidarProductoDelAlmacen());
 
         //leerYValidarDecremento
         System.out.println(funcion.leerYValidarDecremento(producto1));
